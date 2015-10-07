cb2e50a95ff8830565f654bcb3fb66ee348d5574
ex7689 <ex7689@wayne.edu>
2015-10-07 12:45:16 -0400

    Modified to run on all plates of a given cell type

--- expression_pipeline/expr_getMeanAndTopExpr.R
+++ expression_pipeline/expr_getMeanAndTopExpr.R
@@ -5,7 +5,7 @@
 ## and returns a matrix condensed to one transcript per gene
 ##
 ## Required Arguments
-##   platePrefix - character; experiment to run, e.g., DP1
+##   celltype - character; cell type to process, e.g., HUVEC
 ##   word - character; file infix denoting processing steps, etc
 ##
 ## Optional Arguments
@@ -25,16 +25,32 @@ defaultList = list(
   bedTranscriptome="/wsu/home/groups/piquelab/data/RefTranscriptome/ensGene.hg19.2014.bed.gz"
   )
 args <- getArgs(defaults=defaultList)
-platePrefix      <- args$platePrefix # change to cell.type
+#platePrefix      <- args$platePrefix # change to cell.type
+cell.type        <- args$celltype
 word             <- args$word
 cores            <- as.numeric(args$cores)
 bedTranscriptome <- args$bedTranscriptome
+
+## Check for required arguments
+stopifnot(!is.null(cell.type) & !is.null(word))
 
 print(args)
 
 ParallelSapply <- function(...,mc.cores=cores){
   simplify2array(mclapply(...,mc.cores=mc.cores))
 }
+
+##################################################################
+## specify covariate file and pileup directories
+##################################################################
+covDir <- '../../derived_data/covariates/'
+cmd <- paste0('cat ', covDir, 'GxE_DP*_covariates.txt | grep -w ', cell.type)
+cv <- read.table(pipe(cmd), as.is=TRUE, sep='\t')
+names(cv) <- c("Plate.ID", "Barcode.ID", "Raw.Reads", "Quality.Reads", "Clean.Reads",
+                              "Treatment.ID", "Treatment", "BarcodeSequence", "CellType", "CellLine",
+                              "Control.ID", "Control")
+cv$Treatment <- gsub(' ',  '_', cv$Treatment)
+
 
 ## Load the gene annontation file
 anno <- read.table(as.is=T, sep="\t",
@@ -45,21 +61,28 @@ rownames(anno) <- anno$t.id
 
 ## Data comes from the counts_to_rpkms.R script
 ## Assumes colnames are "<indiv>.<cond>"
-load(paste0(platePrefix, '/', platePrefix, '.', word, '.Rd'))
+
+##load(paste0(platePrefix, '/', platePrefix, '.', word, '.Rd'))
+fpkms <- do.call('cbind', lapply(unique(cv$Plate.ID), function(p) {
+  load(paste0(p, '/', p, '.', word, '.Rd'))
+  colnames(fpkms) <- paste(p, colnames(fpkms), sep='.')
+  fpkms
+}))
 
 ## Get the mean expression, across transcripts, per condition
-individuals = unique(gsub("\\..*", "", colnames(fpkms)))
-conditions = unique(gsub(".*\\.", "", colnames(fpkms)))
+## NOTE: Breaks if '.' in cell.line ID (or txID or plateID)
+x <- strsplit(colnames(fpkms), '\\.')
+plates      <- unique(sapply(x, function(y) { y[1] }))
+individuals <- unique(sapply(x, function(y) { y[2] }))
+conditions  <- unique(sapply(x, function(y) { y[3] }))
 meanExpr <- as.data.frame(ParallelSapply(conditions, function(c) {
   apply(fpkms[ , grep(c, colnames(fpkms))], 1, mean)
 }))
 colnames(meanExpr) <- conditions
 
-## Ouput the mean gene expression for each transcript
-stopifnot(sum(rownames(meanExpr) == anno$t.id) == dim(meanExpr)[1])
-meanExprAnno <- as.data.frame(cbind(anno[ , c(1,2,3,4,7,8)], meanExpr))
-outFile = paste0(platePrefix, "/", platePrefix, '.', word, '.meanExpr.all.gz')
-write.table(meanExprAnno, quote=F, sep='\t', row.names=F, file=gzfile(outFile))
+## Add the gene annotation information
+m <- match(rownames(meanExpr), anno$t.id)
+meanExprAnno <- as.data.frame(cbind(anno[m , c(1,2,3,4,7,8)], meanExpr))
 
 ## Get the list of top expressed transcrips
 topTx <- ParallelSapply(unique(meanExprAnno$ensg), function(g){
@@ -67,21 +90,24 @@ topTx <- ParallelSapply(unique(meanExprAnno$ensg), function(g){
       m = apply(aux[, 7:dim(aux)[2]], 1, mean)
       n = names(which( m == max(m) ))
       if (length(n)>1) {
-        #return (sample(n, 1)) 
+        #return (sample(n, 1))
+        print(n)
         n = sample(n, 1) ## If multiple have same avg, choose one randomly
       }
       m[n]
     })
 
-## Save the list of max transcripts and their platewide mean fpkm
-save(topTx, file=paste0(platePrefix, '/', platePrefix, '.', word, '.topTx.Rd'))
-
-## Output the data twice, once for all transcripts, once for the top set
-outFile = paste0(platePrefix, '/', platePrefix, '.', word, '.meanExpr.all.gz')
-write.table(meanExprAnno, row.names=F, quote=F, sep='\t',
-            file=gzfile(outFile))
-outFile = paste0(platePrefix, '/', platePrefix, '.', word, '.meanExpr.perGene.gz')
-write.table(meanExprAnno[names(topTx), ], quote=F, sep='\t', row.names=F,
-  file=gzfile(outFile))
+## Ouput the mean gene expression for each transcript. Split back
+## into separate files per plate
+for ( p in plates ) {
+  tx <- unique(cv$Treatment.ID[ cv$Plate.ID == p ])  
+  outFile = paste0(p, "/", p, '.', word, '.meanExpr.all.gz')
+  write.table(meanExprAnno[, c(colnames(meanExprAnno)[1:6], sort(tx))],
+              quote=F, sep='\t', row.names=F, file=gzfile(outFile))
+  outFile = paste0(p, '/', p, '.', word, '.meanExpr.perGene.gz')
+  write.table(meanExprAnno[names(topTx), ], quote=F, sep='\t', row.names=F,
+              file=gzfile(outFile))
+  save(topTx, file=paste0(p, '/', p, '.', word, '.topTx.Rd'))
+}
 
 ## the end ##
