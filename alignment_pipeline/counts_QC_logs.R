978ad8baaa179a92abd7f44be09059cedaecf506
ex7689 <ex7689@wayne.edu>
2015-10-11 12:14:59 -0400

    Minor fixes, adjusted output tables

--- alignment_pipeline/counts_QC_logs.R
+++ alignment_pipeline/counts_QC_logs.R
@@ -53,13 +53,12 @@ all_counts <- lapply(filters, function(this_filter){
 names(all_counts) <- filters
 
 counts_merged <- MergeList(2, all_counts, mergeCol = 'sample')
-names(counts_merged) <- c('sample', rev(filters))
+names(counts_merged) <- c('Barcode.ID', rev(filters))
 n.barcodes <- dim(counts_merged)[1]
 
-plot_sample <- rep(counts_merged$sample, 3)
+plot_sample <- rep(counts_merged$Barcode.ID, 3)
 plot_count <-c(counts_merged[, 2], counts_merged[, 3], counts_merged[, 4])
 plot_filter <- factor(c(rep('clean', n.barcodes), rep('quality', n.barcodes), rep('merged', n.barcodes)), level=c('merged', 'quality', 'clean'))
-
 counts_long <- data.frame(barcode=as.factor(plot_sample), counts=plot_count, filter=plot_filter)
 
 ## plot
@@ -73,7 +72,6 @@ cov.file <- paste('../../../covariates/GxE_', gsub('DP', 'P', platePrefix),
                   '_covariates.txt', sep='')
 covariates <- read.table(cov.file, header=TRUE, sep='\t')
 cov_short <- covariates[, c('Plate.ID', 'Barcode.ID', 'CellLine', 'Treatment.ID')]
-names(counts_merged) <- c('Barcode.ID', filters[c(3, 2, 1)])
 out_data <- merge(cov_short, counts_merged, by='Barcode.ID')
 
 ## For deep plates, update the Plate.ID. Additionally, write out an abbreviate
@@ -85,14 +83,15 @@ if ( strtrim(platePrefix, 2) == "DP" ) {
                                 c("Plate.ID", "Barcode.ID", "RawReads", "QualityReads",
                                   "CleanReads", "Treatment.ID", "Treatment", "BarcodeSequence",
                                   "CellType", "CellLine", "Control.ID", "ControlCategory") ]
-  deepCovariates$Plate.ID <- platePrefix
-  deepCovariates$RawReads     <- out_data$merged
+  deepCovariates$Plate.ID     <- platePrefix
+  deepCovariates$RawReads     <- out_data$raw
+  ##deepCovariates$RawReads  <- out_data$merged
   deepCovariates$QualityReads <- out_data$quality
   deepCovariates$CleanReads   <- out_data$clean
   write.table(deepCovariates, file=paste0('../../../covariates/GxE_', platePrefix, '_covariates.txt'),
               quote=FALSE, row.names=FALSE, sep='\t')
 }
-write.table(out_data, file=paste(platePrefix, '_QC_counts.txt', sep=''), quote=FALSE, row.names=FALSE, sep='\t')
+write.table(out_data[-grep('raw', colnames(out_data))], file=paste(platePrefix, '_QC_counts.txt', sep=''), quote=FALSE, row.names=FALSE, sep='\t')
 
 ## Finally, output the aggregate sums of raw reads for each barcode/individual
 ## combination to check if we've hit the desired depth
