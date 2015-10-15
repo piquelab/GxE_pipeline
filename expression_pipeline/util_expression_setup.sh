cb12adca5630803b02d9e60179bb34714d35773d
ex7689 <ex7689@wayne.edu>
2015-10-15 12:50:52 -0400

    Updated expression util/setup script

--- expression_pipeline/util_expression_setup.sh
+++ expression_pipeline/util_expression_setup.sh
@@ -5,10 +5,7 @@ set -v
 mkdir -p ../../expression/fpkms/
 mkdir -p ../../expression/DESeq2/
 
-cp util_expression_runAll.sh ../../expression/fpkms/
 cp expr_* ../../expression/fpkms/
-
-cp util_DEG_analysis.sh ../../expression/DESeq2/
 cp DESeq2_* ../../expression/DESeq2/
 
 cp treatmentKey.txt ../../expression/
